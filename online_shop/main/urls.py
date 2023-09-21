94988a9bad13dccf11536c02062310055cbed5ab
Алексей Альхимовичч <138608998+Aleksei-Alkhimovich@users.noreply.github.com>
2023-09-21 04:27:53 +0300

    Проблемы с отоброжением страницы товара

--- online_shop/main/urls.py
+++ online_shop/main/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='home'),
     path('shop', views.shop, name='shop'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_items'),
 ]
--- online_shop/main/views.py
+++ online_shop/main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from django.views.generic import DetailView
 
 
 def index(request):
     return render(request, 'main/index.html')
 
-def create(request):
-    return render(request, 'main/create.html')
+
+class NewsDetailView(DetailView):
+    model = Product
+    template_name = 'main/detail_items.html'
+    context_object_name = 'article'
+
 
 def shop(request):
     product = Product.objects.all()
@@ -14,5 +19,3 @@ def shop(request):
         'pr': product
     }
     return render(request, 'main/shop.html', context)
-
-
